3fd020193710b1e3d0261a932dd2903b2c8ed7a4
yduojia <yduojia@vmware.com>
2020-09-21 11:57:21 +0800

    add search filter

--- brain/src/components/Matrix/components/DTable.vue
+++ brain/src/components/Matrix/components/DTable.vue
@@ -314,7 +314,13 @@ export default {
       if(!searchString) return []
       for(let data of this.matrixDisplayData){
         console.log(data);
-        if(data.device_name.slice(0,searchString.length).toLowerCase() === searchString.toLowerCase() || data.Horizon_client_version.slice(0,searchString.length) === searchString || data.Horizon_agent_version.slice(0,searchString.length) === searchString){
+        if(data.device_name.slice(0,searchString.length).toLowerCase() === searchString.toLowerCase() || 
+        data.Horizon_client_version.slice(0,searchString.length) === searchString || 
+        data.Horizon_agent_version.slice(0,searchString.length) === searchString||
+        data.vendor_id.slice(0,searchString.length) == searchString ||  
+        data.product_id.slice(0,searchString.length) === searchString||
+        data.model.slice(0,searchString.length).toLowerCase() === searchString.toLowerCase())
+        {
           res.push(data)
         }
       }
