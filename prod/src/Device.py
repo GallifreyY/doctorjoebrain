87bc14771ff584bf77ac42fb564974cbfceb1091
yduojia <yduojia@vmware.com>
2020-09-25 14:11:01 +0800

    revert back

--- prod/src/Device.py
+++ prod/src/Device.py
@@ -40,17 +40,18 @@ class Device:
     def _is_usb_redirect(self):
         # todo:details
 
-        if self.type == 'usbdisk' or self.type == 'scanners' or self.type == 'cameras' or self.type == 'printers':
+        if self.type == 'usbdisk' or self.type == 'scanners' or self.type == 'cameras' :
             if self.end == 'agent':
                 return True
             else:
                 return False
-'''
-        elif self.type == 'printers':
-            # printer will be detected only in agent
-            if self.vid is not None and self.pid is not None:
-                return False
 
+        elif self.type == 'printers'and self.end == 'agent':
+            if self.vid is not None and self.pid is not None:
+                return True
+            else:
+                return False
+        elif self.type == 'printers' and self.end == 'client':
             for device_r in self.raw_data['agent'][self.type]:
                 if 'VID' in device_r.keys() and 'PID' in device_r.keys():
                     # device_id = device_r['VID'] + '-' + device_r['PID']
@@ -71,7 +72,7 @@ class Device:
                     #     return True
 
         return False
-'''
+
     def find_details(self):
         devices = self.raw_data[self.end][self.type]
         return devices[self.index] if len(devices) >= self.index else None
