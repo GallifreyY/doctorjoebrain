99c69dc5cd8502ae1ff2432e3e7806a33a3f208d
yduojia <yduojia@vmware.com>
2020-09-25 16:18:43 +0800

    add blamkspace

--- back-end/src/Device.py
+++ back-end/src/Device.py
@@ -46,7 +46,7 @@ class Device:
             else:
                 return False
 
-        elif self.type == 'printers'and self.end == 'agent':
+        elif self.type == 'printers' and self.end == 'agent':
             if self.vid is not None and self.pid is not None:
                 return True
             else:
@@ -54,7 +54,7 @@ class Device:
         elif self.type == 'printers' and self.end == 'client':
             if self.vid is not None and self.pid is not None:
                 return False
-                
+
             for device_r in self.raw_data['agent'][self.type]:
                 if 'VID' in device_r.keys() and 'PID' in device_r.keys():
                     # device_id = device_r['VID'] + '-' + device_r['PID']
