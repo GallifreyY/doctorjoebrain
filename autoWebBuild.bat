354b868b5f79c194d18c7352310dc0c470a53cc5
Duojia Yang <yduojia@vmware.com>
2021-03-04 14:04:53 +0800

    rename and delete pause

--- autoWebBuild.bat
+++ autoWebBuild.bat
@@ -3,6 +3,6 @@ call npm run build
 cd ..
 
 set winscp="C:\Program Files (x86)\WinSCP\WinSCP.exe"
-pause
+
 %winscp% /console /command "option confirm off" "open root:vmwareca$hc0w@testhpi.eng.vmware.com:22" "put .\brain\dist /home/doctorjoe/prod/" "put .\back-end\src /home/doctorjoe/prod/" "call service appstart restart" "exit" /log=log_winscp.txt
 
